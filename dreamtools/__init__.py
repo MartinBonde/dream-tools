fb64aeb51cd4032fdc6642e09755580d53d7d30e
Martin Kirk Bonde <martin@bonde.dk>
2020-03-11 10:19:51 +0100

    Fixed bug where dream-tools could not be imported without having the plotly package installed. Plotly functionality should now be properly encapsulated (to be split off into its own module at a later date).

--- dreamtools/__init__.py
+++ dreamtools/__init__.py
@@ -38,54 +38,51 @@ def time(start, end=None):
   END_YEAR = end
 
 
-def add_trace_pr_column(fig, df, start=None, end=None, go_type=go.Scatter):
-  if start is None:
-    start = START_YEAR
-  if end is None:
-    end = END_YEAR
-  df = df[start < df.index]
-  df = df[df.index <= end]
-  for name in df:
-    fig.add_trace(go_type(x=df.index, y=df[name], name=name))
-
-
-def multiseries_figure(*series, title=None, start=None, end=None):
-  fig = go.Figure()
-  if title:
-    fig.layout = go.Layout(title=go.layout.Title(text=title, xanchor='center'))
-  add_trace_pr_column(fig, merge_multiseries(*series, keep_axis_index=X_AXIS_INDEX), start, end)
-  return fig
-
-
-def plot(*series, start=None, end=None, title=None, renderer=None, file=None):
-  fig = multiseries_figure(*series, title=title, start=start, end=end)
-  if file is None:
-    fig.show(renderer=renderer)
-  else:
-    fig.write_image(file)
-
-def _series_plotly(self, start=None, end=None, title=None, **kwargs):
-  if title is None:
-    title = self.name
-  plot(self, start=start, end=end, title=title, **kwargs)
-
-def _series_plot(self):
-  if PLOTLY:
-    _series_plotly()
-pd.Series.plot = _series_plot
-
-
-def _data_frame_plotly(self, start=None, end=None, title=None, renderer=None, file=None):
-  fig = go.Figure()
-  if title:
-    fig.layout = go.Layout(title=go.layout.Title(text=title))
-  add_trace_pr_column(fig, self, start, end)
-  if file is None:
-    fig.show(renderer=renderer)
-  else:
-    fig.write_image(file)
-
 if PLOTLY:
+  def add_trace_pr_column(fig, df, start=None, end=None, go_type=go.Scatter):
+    if start is None:
+      start = START_YEAR
+    if end is None:
+      end = END_YEAR
+    df = df[start < df.index]
+    df = df[df.index <= end]
+    for name in df:
+      fig.add_trace(go_type(x=df.index, y=df[name], name=name))
+
+
+  def multiseries_figure(*series, title=None, start=None, end=None):
+    fig = go.Figure()
+    if title:
+      fig.layout = go.Layout(title=go.layout.Title(text=title, xanchor='center'))
+    add_trace_pr_column(fig, merge_multiseries(*series, keep_axis_index=X_AXIS_INDEX), start, end)
+    return fig
+
+
+  def plot(*series, start=None, end=None, title=None, renderer=None, file=None):
+    fig = multiseries_figure(*series, title=title, start=start, end=end)
+    if file is None:
+      fig.show(renderer=renderer)
+    else:
+      fig.write_image(file)
+
+  def _series_plotly(self, start=None, end=None, title=None, **kwargs):
+    if title is None:
+      title = self.name
+    plot(self, start=start, end=end, title=title, **kwargs)
+
+  pd.Series.plot = _series_plotly
+
+
+  def _data_frame_plotly(self, start=None, end=None, title=None, renderer=None, file=None):
+    fig = go.Figure()
+    if title:
+      fig.layout = go.Layout(title=go.layout.Title(text=title))
+    add_trace_pr_column(fig, self, start, end)
+    if file is None:
+      fig.show(renderer=renderer)
+    else:
+      fig.write_image(file)
+
   pd.DataFrame.plot = _data_frame_plotly
 
 
