6a58c95efb108fde9ab7e4633840c5cc89445b4d
Martin Kirk Bonde <martin@bonde.dk>
2023-01-07 08:13:12 +0100

    Breaking changes. Plot, print and to_dataframe now also accepts databases as inputs if a function is supplied (function receives database as input). Default aggregation is no longer based on reference database, but instead always aggregates as much as possible. reference_database property added to GamsPandasDatabase, used to specify a baseline database to be used with another database.

--- dreamtools/multiindex_plotly/test_timeseries_analysis.py
+++ dreamtools/multiindex_plotly/test_timeseries_analysis.py
@@ -1,16 +1,16 @@
 import os
-
 import sys
 sys.path.insert(0, os.getcwd())
+os.chdir("../..")
 
+import pytest
 import numpy as np
 import pandas as pd
 import dreamtools as dt
 
-dt.REFERENCE_DATABASE = r = dt.Gdx("test.gdx")
-s = dt.Gdx("test.gdx")
-
-def test_to_dataframe():
+def test_to_dataframe_with_series():
+  dt.REFERENCE_DATABASE = dt.Gdx("test.gdx")
+  s = dt.Gdx("test.gdx")
   dt.time(2025, 2040)
   assert dt.to_dataframe(s.qBNP).shape == (16, 1)
   assert dt.to_dataframe(s.qY).shape == (16, 1)
@@ -19,4 +19,36 @@ def test_to_dataframe():
   s.foo = s.qY.loc[s.sp]
   s.foo.name = "foo"
   assert dt.to_dataframe(s.foo).shape == (16, 8)
-  dt.to_dataframe(s.foo, "q")
+  with pytest.raises(KeyError):
+    dt.to_dataframe(s.foo, "q")
+
+def test_to_dataframe_with_database():
+  dt.REFERENCE_DATABASE = dt.Gdx("test.gdx")
+  s = dt.Gdx("test.gdx")
+  dt.time(2025, 2040)
+  assert dt.to_dataframe(s, function = lambda s: s.qBNP).shape == (16, 1)
+  assert dt.to_dataframe(s, function = lambda s: s.qY).shape == (16, 1)
+  assert dt.to_dataframe(s, function = lambda s: s.qY[s.s]).shape == (16, 9)
+  assert dt.to_dataframe(s, "q", lambda s: s.qY[s.s]).shape == (16, 9)
+  assert dt.to_dataframe(s, "q", lambda s: s.pY[s.s] * s.qY[s.s]).shape == (16, 9)
+  
+def test_to_dataframe_with_multiple_baselines():
+  b1 = dt.Gdx("test.gdx")
+  b2 = dt.Gdx("test.gdx")
+  s1 = dt.Gdx("test.gdx")
+  s2 = dt.Gdx("test.gdx")
+  s1.reference_database = b1
+  s2.reference_database = b2
+  b1.qY *= 2
+  b2.qY /= 2
+  dt.time(2025, 2040)
+  assert dt.to_dataframe([s1, s2], "q", lambda s: s.pY[s.s] * s.qY[s.s]).shape == (16, 9*2)
+  q = dt.to_dataframe([s1, s2], "q", lambda s: s.pY['tje'] * s.qY['tje'], ["s1", "s2"])
+  assert all(q["s1"] == -0.5)
+  assert all(q["s2"] == 1)
+
+  s1 = dt.Gdx("test.gdx")
+  s2 = dt.Gdx("test.gdx")
+  q = dt.to_dataframe([s1, s2], "q", lambda s: s.pY['tje'] * s.qY['tje'], ["s1", "s2"], baselines=[b1, b2])
+  assert all(q["s1"] == -0.5)
+  assert all(q["s2"] == 1)
