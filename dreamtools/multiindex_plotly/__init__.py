d0c45b8e2969423e875a75109b0e21e983647189
Martin Kirk Bonde <martin@bonde.dk>
2023-03-02 11:23:08 +0100

    Plotly added as dependency

--- dreamtools/multiindex_plotly/__init__.py
+++ dreamtools/multiindex_plotly/__init__.py
@@ -1,19 +1,14 @@
-try:
-  import plotly
-  PLOTLY = True
-except ImportError:
-  PLOTLY = False
+import plotly.io as pio
+import pandas
+pandas.options.plotting.backend = "plotly"
+from .timeseries_analysis import *
+from .reporting import *
+from .age_plot import *
 
-if PLOTLY:
-  import pandas
-  pandas.options.plotting.backend = "plotly"
-  from .timeseries_analysis import *
-  from .reporting import *
-  from .age_plot import *
+from .dream_plotly_template import dream_template
+pio.templates["dream"] = dream_template
+pio.templates.default = "dream"
 
-  import plotly.io as pio
-  from .dream_plotly_template import dream_template
-  pio.templates["dream"] = dream_template
-  pio.templates.default = "dream"
+from .dream_plotly_template import dream_colors_rgb, small_figure_layout, large_figure_layout
 
-  from .dream_plotly_template import dream_colors_rgb, small_figure_layout, large_figure_layout
+
