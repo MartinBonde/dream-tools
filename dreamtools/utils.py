3503a43b5ea3cf60d1e8dc7028fe6dff29c29afb
Martin Kirk Bonde <martin@bonde.dk>
2025-04-03 13:40:00 +0200

    Add max iteration safeguard in find_root in utils.py

--- dreamtools/utils.py
+++ dreamtools/utils.py
@@ -2,24 +2,22 @@ from pathlib import Path
 import glob
 import shutil
 
-def find_root(root_file_name="paths.py"):
+def find_root(root_file_name="paths.py", max_iter=20):
     """
     Find the root of the project by looking for a file named root_file_name.
     
     Parameters:
     root_file_name (str): The name of the file to look for (default is "paths.py").
+    max_iter (int): Maximum number of parent directories to check before giving up.
     
     Returns:
     str: The path to the directory containing root_file_name, or None if not found.
     """
-    current_path = Path.cwd()
-    root_path = current_path.root
-
-    while current_path != root_path:
-        if (current_path / root_file_name).is_file():
-            return str(current_path)
-        current_path = current_path.parent
-
+    current = Path.cwd()
+    for _ in range(max_iter):
+        if (current / root_file_name).exists():
+            return str(current)
+        current = current.parent
     return None
 
 def clean_gams_temp_files():
